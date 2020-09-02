3b14ae02af4c34ba1b47cea7f1053250ad44d7fe
IndigoDreams88 <svidler2011@gmail.com>
2020-09-02 12:27:22 +0100

    updated main page description

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -13,12 +13,10 @@ function LeftPanel() {
           <h1 className={styles.titleText}>Sarah Vidler</h1>
           <h2 className={styles.titleText2}> = Coder = ~ Designer ~</h2>
           <p className={styles.welcomeText}>
-            I've recently graduated from the Northcoders coding Bootcamp on
-            07/02/2020. It's been an amazing few months and I have learned many
-            new skills, most importantly the power of self belief and not giving
-            in to the "imposter syndrome" that is so prevalent in the tech
-            industry. I can't wait to begin a new career in tech, and I am
-            currently looking for career opportunities.{" "}
+            I graduated from the Northcoders coding Bootcamp on 07/02/2020. It
+            was a great course and I learned many new skills, most importantly
+            the power of self belief and not giving in to the "imposter
+            syndrome" that is so prevalent in the tech industry.{" "}
           </p>
           <Icons />
         </center>
